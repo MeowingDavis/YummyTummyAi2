218cf80f9e8e84525be96f44c6ad4ecf7db15737
Davis Lang <s3775656@student.rmit.edu.au>
2025-05-20 21:56:47 +1000

    Update html.ts

--- html.ts
+++ html.ts
@@ -5,6 +5,7 @@ export default `
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Yummy Tummy Ai</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <div id="container">
